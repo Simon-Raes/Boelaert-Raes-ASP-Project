2c3b8e0160de65a48844b142ecfb3eb3098320e2
Serafijn <serafijn.boelaert@gmail.com>
2014-04-08 08:34:21 +0200

    Menu wordt dynamisch opgemaakt
    Het menu dat alle genres en categoriën toont, wordt nu dynamisch
    opgemaakt

--- Boelaert-Raes-Project/LayeredBusinessModel.WebUI/MasterPage.Master.designer.cs
+++ Boelaert-Raes-Project/LayeredBusinessModel.WebUI/MasterPage.Master.designer.cs
@@ -67,6 +67,15 @@ namespace LayeredBusinessModel.WebUI {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
+        /// dvMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvMenu;
+        
+        /// <summary>
         /// arBanner control.
         /// </summary>
         /// <remarks>
